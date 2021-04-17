2878be6998ac5c7a9ea77f0622c3eb52d113fe2d
hirvapipaliya29 <hirvapipaliya6869@gmail.com>
2021-04-17 11:57:23 +0530

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+import 'stylesheets/application'
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+git config --global color.ui true
+git config --global user.name "hirvapipaliya29"
+git config --global user.email "hirvapipaliya6869@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "hirvapipaliya6869@gmail.com"
